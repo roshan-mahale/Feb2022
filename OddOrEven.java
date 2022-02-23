72b9f8495d20f0cbb78d3526f885adfc6295638e
roshan-mahale <99242453+roshan-mahale@users.noreply.github.com>
2022-02-23 11:49:33 +0530

    Update OddOrEven.java

--- OddOrEven.java
+++ OddOrEven.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class OddOrEven {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        int n;
-        System.out.println("Enter No.: ");
-        n = obj.nextInt();
-        
-        if (n%2==0) {
-            System.out.println("The number "+n+" is Even.");
+        try (Scanner obj = new Scanner(System.in)) {
+            int n;
+            System.out.println("Enter No.: ");
+            n = obj.nextInt();
+            
+            if (n%2==0) {
+                System.out.println("The number "+n+" is Even.");
 
-        } else {
-            System.out.println("The number "+n+" is Odd.");
+            } else {
+                System.out.println("The number "+n+" is Odd.");
+            }
         }
     }
     
