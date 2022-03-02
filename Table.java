b7446bc67ffbf01db4ab50887019881b65c5ccf9
roshan-mahale <99242453+roshan-mahale@users.noreply.github.com>
2022-03-02 14:34:51 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -2,21 +2,14 @@ import java.util.Scanner;
 
 public class Table {
     public static void main(String[] args) {
-    try (Scanner obj = new Scanner(System.in)) {
+   Scanner obj = new Scanner(System.in);
         System.out.println("Enter no. : ");
         
-        int n = obj.nextInt();
+        int n;
+        n = Integer.parseInt(obj.nextLine());
 
         for (int i = 1; i<=10; i++) {
-            System.out.println(i*n);
-      
-        
+            System.out.println(n+"x"+i+"="+i*n);
       }
     }
-        
-        
-    }
-
-    
-    
 }
