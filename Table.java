1ffd2d8070137f92ecfb79b9c741f86eb385c092
roshan-mahale <99242453+roshan-mahale@users.noreply.github.com>
2022-03-10 19:18:53 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class Table {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        int n;
+        try (Scanner obj = new Scanner(System.in)) {
+            int n;
    try  {
-    System.out.println("Enter no. : ");
-    
-    
-    n = Integer.parseInt(obj.nextLine());
+   System.out.println("Enter no. : ");
+   
+   
+   n = Integer.parseInt(obj.nextLine());
 
-    for (int i = 1; i<=10; i++) {
-        System.out.println(n+"x"+i+"="+i*n);
-     }
+   for (int i = 1; i<=10; i++) {
+            System.out.println(n+"x"+i+"="+i*n);
+    }
 } catch (NumberFormatException e) {
-    System.out.println("Number Allowed Only");
+   System.out.println("Number Allowed Only");
 } finally{
-    obj.nextLine();
+   obj.nextLine();
 }
+        }
     }
 }
