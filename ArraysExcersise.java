d96c8c17b351285e5b9873813d30808ca6f3d63d
roshan-mahale <99242453+roshan-mahale@users.noreply.github.com>
2022-03-12 23:48:11 +0530

    Update ArraysExcersise.java

--- ArraysExcersise.java
+++ ArraysExcersise.java
@@ -1,20 +1,35 @@
 import java.util.Arrays;
 
 public class ArraysExcersise {
+     static int i;
+
     public static void main(String[] args) {
-        int [] myArray1 = { 22, 33, 52, 83, 48, 99, 403, 49, 56, 79};
+        int [] myArray1 = { 42, 33, 52, 83, 48, 99, 403, 49, 56, 79};
 
         String [] myArray2 ={"john", "steven", "tony", "monica", "smith", "jasmine"};
         //print all arrays
        System.out.println("All array1 are: "+Arrays.toString(myArray1));
         System.out.println("All array2 are: "+Arrays.toString(myArray2));
-
+        
+        
        //Print Sort arrays
        Arrays.sort(myArray1);
        System.out.println("All Sort array1 are: "+Arrays.toString(myArray1));
        Arrays.sort(myArray2);
        System.out.println("All Sort array12 are: "+Arrays.toString(myArray2));
 
+        //Print all arrays in reverse
+       int l = myArray1.length;
+       int n = Math.floorDiv(l, 2);
+       int temp;
+       for (int i=0; i<n; i++){
+        //Swap with temparary variable
+        temp = myArray1[i];
+        myArray1[i]= myArray1[l-i-1];
+        myArray1[l-i-1] = temp;
+       }
+       for (int element:myArray1)
+       System.out.print("all the"+ element+" ");
 
         
     }
